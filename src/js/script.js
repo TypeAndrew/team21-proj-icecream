b8222a07a608d4e142f871fb24358abd3b6c2686
VladimirGvv <vovagvv69@gmail.com>
2022-08-13 14:18:13 +0300

    add slick slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,11 @@
 $(document).ready(function(){
 	$('.slider').slick({
-		arrows:true,
+		arrows:false,
 		dots:true,
-		slidesToShow:3,
-		autoplay:true,
+		slidesToShow:1,
+		autoplay:false,
 		speed:1000,
 		autoplaySpeed:800,
-		responsive:[
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow:2
-				}
-			},
-			{
-				breakpoint: 550,
-				settings: {
-					slidesToShow:1
-				}
-			}
-		]
 	});
 });
 
