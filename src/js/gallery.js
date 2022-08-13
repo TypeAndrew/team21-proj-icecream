7416046fa3ddcc0c2c17b3df0a759ac1c64dff64
shulyar <andy.shular@gmail.com>
2022-08-13 11:46:53 +0300

    make small animation, but not enouf

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -16,5 +16,28 @@ window.onload = () => {
 
 
 };
+const cards = document.querySelectorAll('.gallery__grid-item');
+
+cards.forEach(card => {
+  card.addEventListener('mousemove', onMousemoveCardHandler);
+  card.addEventListener('mouseleave', onMouseleaveCardHandler);
+});
+
+function onMousemoveCardHandler(evt) {
+  const cardInner = this.querySelector('.gallery__grid-img');
+  const halfHeight = cardInner.offsetHeight / 8;
+  const halfWidth = cardInner.offsetWidth / 8;
+  const y = evt.offsetY;
+  const x = evt.offsetX;
+  cardInner.style.transform = `rotateX(${(-y + halfHeight) / 5}deg) rotateY(${
+    (x - halfWidth) / 4
+  }deg)`;
+}
+
+function onMouseleaveCardHandler(evt) {
+  const cardItem = this.querySelector('.gallery__grid-img');
+
+  cardItem.style.transform = '';
+}
 
   
