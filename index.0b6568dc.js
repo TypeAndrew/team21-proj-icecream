800acdde4e8cb44bb355848a70fe2c9eee5917af
TypeAndrew <69585440+TypeAndrew@users.noreply.github.com>
2022-08-19 11:43:22 +0000

    Deploying to gh-pages from @ TypeAndrew/team21-proj-icecream@2f3c5948827c624c69a4e4bfe4d60ef727c81662 🚀

--- index.0b6568dc.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-contacts-open]"),closeModalBtn:document.querySelector("[data-modal-contacts-close]"),modal:document.querySelector("[data-modal-contacts]")};function t(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",t),o.closeModalBtn.addEventListener("click",t)}();
-//# sourceMappingURL=index.0b6568dc.js.map
--- index.eb66b623.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0,slidesToShow:1,autoplay:!0,speed:1e3,autoplaySpeed:800})}));
-//# sourceMappingURL=index.eb66b623.js.map
